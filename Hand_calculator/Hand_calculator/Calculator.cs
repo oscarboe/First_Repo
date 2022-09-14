369bad2711ac6f66eadd6c71ac426a158e55b3ae
Kappel <magnus.kappel@icloud.com>
2022-09-14 15:38:27 +0200

    Lala :-D

--- Hand_calculator/Hand_calculator/Calculator.cs
+++ Hand_calculator/Hand_calculator/Calculator.cs
@@ -4,28 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
 namespace Hand_calculator
 {
     public class Calculator
     {
+        public double Accumulator{get; private set;}
+
         public double Add(double a, double b)
         {
+            Accumulator+=a+b;
             return a + b;
         }
         public double Subtract(double a, double b)
         {
+            Accumulator+= a-b;
             return(a - b);
         }
         public double Multiply(double a, double b)
         {
+            Accumulator+=a*b;
             return (a * b);
         }
 
         public double Power(double x, double exp)
         {
-            return (Math.Pow(x,exp));
+            Accumulator+=Math.Pow(x,exp);
+            
+            //var Hand_calculator new Accumulator(); 
+            return Math.Pow(x,exp);
     
         }
+
 
 
     }
